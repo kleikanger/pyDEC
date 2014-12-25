80e6a173746801b86401ad499861d7dca64950c3
Karl Roald Leikanger <kleikanger@gmail.com>
2014-12-25 23:22:37 +0100

    lots of changes

--- Basis.py
+++ Basis.py
@@ -1,15 +1,17 @@
 
 '''
-@file Contains a class that reads and writes basisfiles in both CP2K and DALTON format.
-@brief Read basis file, store basis info and generate basis input to CP2K or DALTON
+@file Contains a class that reads/writes basisfiles in both CP2K/DALTON format.
+@brief Read basis file, store basis info and generate basis input to CP2K or
+DALTON
 @date 2014
 @author Karl R. Leikanger.
 
 The orbitals are read from file and stored in the following format:
-    -chem_elems is an array of __ChemElemBasis objects. Contains atomic number 
-    (chem_elems.atomic_nr) and an list of __AtomicOrbital objects (chem_elems.ao), 
-    -each of these have a list of coeffs (oa.c), a list of exponents (ao.e) and 
-    a AM quantum number (ao.l) stored.  
+    -chem_elems is an array of __ChemElemBasis objects. Contains atomic number
+    (chem_elems.atomic_nr) and an list of __AtomicOrbital objects
+    (chem_elems.ao),
+    -each of these have a list of coeffs (oa.c), a list of exponents (ao.e) and
+    a AM quantum number (ao.l) stored.
 
 ex:
 filename = '6-311G**'
@@ -21,17 +23,18 @@ import sys
 import parameters as param
 from IO import ReadFile
 
+
 class Basis():
     '''
     @brief
-    @var chem_elems list of class __ChemElemBasis objects. Basis for given atomic nr. 
+    @var chem_elems list of class __ChemElemBasis objects. One for each element.
     @var chem_elems_indx dictionary {atomic_nr : index in chem_elems }
     @var basis_name Name of basis.
     @date 2014
     @author Karl R. Leikanger.
     '''
-    chem_elems = '' 
-    chem_elems_indx = '' 
+    chem_elems = ''
+    chem_elems_indx = ''
     basis_name = ''
     infile_name = ''
 
@@ -42,7 +45,7 @@ class Basis():
         '''
         self.basis_name = basis_name
         self.chem_elems = []
-    
+
     class __ChemElemBasis():
         '''
         @brief Container class. Stores basis info for a given element.
@@ -51,7 +54,7 @@ class Basis():
         @date 2014
         @author Karl R. Leikanger.
         '''
-        atomic_nr = '' 
+        atomic_nr = ''
         ao = ''
         tags_permutations = ''
 
@@ -59,7 +62,6 @@ class Basis():
             self.ao = []
             self.tags_permutations = []
 
-    
     class __AtomicOrbital():
         '''
         @brief Container class. Stores basis info for a given element.
@@ -92,9 +94,10 @@ class Basis():
         if basis_format in ['DALTON', 'LSDALTON']:
             self.__read_dalton_basisfile()
         else:
-            print('Error: Basis input format <%s> not supported.'%basis_format)
+            print('Error: Basis input format <%s> not supported.'
+                  % basis_format)
             sys.exit(-1)
-    
+
     def print_basisfile(self, outputfile_name, basis_format):
         '''
         @brief Print basis basisfile.
@@ -108,10 +111,10 @@ class Basis():
         if basis_format == 'CP2K':
             self.__print_cp2k_basisfile(outputfile_name)
         else:
-            print('Error: Output basis format <%s> not supported.'%basis_format)
+            print('Error: Output basis format <%s> not supported.'
+                  % basis_format)
             sys.exit(-1)
 
-    
     def __read_dalton_basisfile(self):
         '''
         @brief Read a basis input file.
@@ -119,20 +122,19 @@ class Basis():
         @author Karl R. Leikanger.
         '''
 
-        # get_words is a "functor" which returns 'words' of the lines 
+        # get_words is a "functor" which returns 'words' of the lines
         # of infile_name, one line at a time.
-        
+
         print('Reading basis from:', self.infile_name)
         cmarkers = ['!', '$']
         get_words = ReadFile(self.infile_name, cmarkers).get_words_of_line
-        
+
         l = -1
         elem = ''
-        aos = ''
 
-        #iterating over lines in infile_name 
+        # iterating over lines in infile_name
         for words in iter(get_words, ''):
-            if words[0][0] == 'a' or words[0][0] == 'A': 
+            if words[0][0] == 'a' or words[0][0] == 'A':
                 if l > -1:
                     self.chem_elems.append(elem)
                 tag = 1
@@ -142,13 +144,15 @@ class Basis():
                 words = get_words()
             else:
                 l += 1
-            
+
             num_exp = int(words[0])
             num_coef = int(words[1])
             # ? = words[2]
 
-            #Store exponents and contraction coeffs to e and c
-            e = []; c = []; i = 0
+            # Store exponents and contraction coeffs to e and c
+            e = []
+            c = []
+            i = 0
             for i in range(num_exp):
                 words = get_words()
                 e.append(float(words[0]))
@@ -157,48 +161,50 @@ class Basis():
                     while True:
                         words = get_words()
                         c[-1] = c[-1] + [float(x) for x in words[:]]
-                        if len(c[-1]) == num_coef: break
+                        if len(c[-1]) == num_coef:
+                            break
 
-            #set up self.chem_elems.ao, remove coeffs equal to 0
+            # set up self.chem_elems.ao, remove coeffs equal to 0
             for i in range(num_coef):
                 ao = self.__AtomicOrbital()
                 ao.c = [c[j][i] for j in range(num_exp) if c[j][i] != 0.0]
                 ao.e = [e[j] for j in range(num_exp) if c[j][i] != 0.0]
                 ao.l = l
-                ao.tag = tag; tag += 1
+                ao.tag = tag
+                tag += 1
                 elem.ao.append(ao)
 
         self.chem_elems.append(elem)
 
-        #sort and check for duplicates
+        # sort and check for duplicates
         self.__sort_chem_elems()
 
-
-    def __sort_chem_elems(self):   
+    def __sort_chem_elems(self):
         '''
         @brief Sort chem elem after atomic numbers and check for duplicates.
         @date 2014
         @author Karl R. Leikanger.
         '''
-        #set up list of all Chemical element in the basis
+        # set up list of all Chemical element in the basis
         elemset = []
         for elem in self.chem_elems[:]:
-            elemset += [ elem.atomic_nr ]
+            elemset += [elem.atomic_nr]
 
-        #Make sure that the atomic_nr are unique for all chem_elems 
+        # Make sure that the atomic_nr are unique for all chem_elems
         if len(elemset) > len(set(elemset)):
             print('Error: Check input basisfile format.')
             print('Some chem_elems represented more that once.')
             sys.exit(-1)
-        
-        #Make sure that chem_elems ars sorted after atomic number 
-        if (sorted(elemset) != elemset):
-            self.chem_elems = sorted(self.chem_elems, \
-                    key=lambda __ChemElemBasis: __ChemElemBasis.atomic_nr)
 
-        #set up dict { chem_elems.atomic_nr[i] : i }
+        # Make sure that chem_elems ars sorted after atomic number
+        if (sorted(elemset) != elemset):
+            self.chem_elems = sorted(
+                self.chem_elems,
+                key=lambda __ChemElemBasis: __ChemElemBasis.atomic_nr)
+
+        # set up dict { chem_elems.atomic_nr[i] : i }
         self.chem_elems_indx = \
-                dict(zip(sorted(elemset), range(len(elemset))))
+            dict(zip(sorted(elemset), range(len(elemset))))
 
     def __print_cp2k_basisfile(self, filename):
         '''
@@ -208,16 +214,16 @@ class Basis():
         @author Karl R. Leikanger.
 
         '''
-        #open ofilestream
-        f = open(filename,'w')
+        # open ofilestream
+        f = open(filename, 'w')
         f.write('#\n# CP2K input file.\n')
         f.write('# This file is automatically generated by PROGNAME.\n')
-        f.write('# from the DALTON input file %s.\n'%self.infile_name)
-        print('Writing basis in CP2K format to file: %s'%filename)
+        f.write('# from the DALTON input file %s.\n' % self.infile_name)
+        print('Writing basis in CP2K format to file: %s' % filename)
 
         for elem in self.chem_elems:
             self.__print_elem_to_cp2k_basisfile(elem, f)
-        
+
         f.close()
 
     def __print_elem_to_cp2k_basisfile(self, elem, f):
@@ -228,62 +234,65 @@ class Basis():
         @date 2014
         @author Karl R. Leikanger.
 
-        Orbitals are written to file in a specific order. 
+        Orbitals are written to file in a specific order.
         They are sorted after:
             - Their exponents arrays.
             - A.M (within each exponents array).
 
         '''
-        #sort the ao's after the exponents arrays
+        # sort the ao's after the exponents arrays
         aos = sorted(elem.ao, key=lambda __AtomicOrbital: __AtomicOrbital.e)
 
-        #extract the unique exponents arrays
+        # extract the unique exponents arrays
         exp_unique = []
         for ao in aos:
             if ao.e not in exp_unique:
                 exp_unique.append(ao.e)
-            
-        ##if any exponents are equal; combine exponents arrays
-        #repeat = True
-        #while repeat:
-        #    repeat = False
-        #    for i in range(len(exp_unique)):
-        #        for j in range(i+1, range(len(exp_unique))):
-        #            si = set(exp_unique[i])
-        #            sj = set(exp_unique[j])
-        #            if si.intersection(sj) != {}:
-        #                #combine exp_unique[i] exp_unique[j] 
-        #                i = j = len(exp_unique)
-        #                exp_unique[i] = list(si.union(sj))
-        #                exp_unique.pop(j)
-        #                repeat = True
 
-        #print Element symbol Name of the basis set  Alias names
-        elem_symbol = param.elem_symbol_from_a.get(elem.atomic_nr) 
+        '''
+        # if any exponents are equal; combine exponents arrays
+        repeat = True
+        while repeat:
+            repeat = False
+            for i in range(len(exp_unique)):
+                for j in range(i+1, range(len(exp_unique))):
+                    si = set(exp_unique[i])
+                    sj = set(exp_unique[j])
+                    if si.intersection(sj) != {}:
+                        #combine exp_unique[i] exp_unique[j]
+                        i = j = len(exp_unique)
+                        exp_unique[i] = list(si.union(sj))
+                        exp_unique.pop(j)
+                        repeat = True
+        '''
+        # print Element symbol Name of the basis set  Alias names
+        elem_symbol = param.elem_symbol_from_a.get(elem.atomic_nr)
         f.write('#\n# ----------------- \n#\n')
-        f.write('%s %s \n'%(elem_symbol, self.basis_name))
-        #print nset (repeat the following block of lines nset times)
-        f.write('%i\n'%(len(exp_unique)))
+        f.write('%s %s \n' % (elem_symbol, self.basis_name))
+        # print nset (repeat the following block of lines nset times)
+        f.write('%i\n' % (len(exp_unique)))
 
         indx_aos = 0
         for i in range(len(exp_unique)):
 
-            #collect the aos with the same exponents arrays
+            # collect the aos with the same exponents arrays
             aos_set = []
             while aos[indx_aos].e == exp_unique[i]:
-            #while set(aos[indx_aos].e).intersection(set(exp_unique[i])):
+                # while set(aos[indx_aos].e).intersection(set(exp_unique[i])):
                 aos_set.append(aos[indx_aos])
                 indx_aos += 1
-                if (indx_aos==len(aos)): break #error
+                if (indx_aos == len(aos)):
+                    break  # error
 
-            #sort aos after l
-            aos_set = sorted(aos_set, key=lambda __AtomicOrbital: __AtomicOrbital.l)
+            # sort aos after l
+            aos_set =\
+                sorted(aos_set, key=lambda __AtomicOrbital: __AtomicOrbital.l)
 
-            #collect min and max A.M.
-            lmin =  aos_set[0].l
-            lmax =  aos_set[-1].l
+            # collect min and max A.M.
+            lmin = aos_set[0].l
+            lmax = aos_set[-1].l
 
-            #sort coefs after A.M. (in c_l)
+            # sort coefs after A.M. (in c_l)
             c_l = []
             tmp = []
             ltmp = lmin
@@ -295,27 +304,27 @@ class Basis():
                 tmp.append(aos_set[j].c)
             c_l.append(tmp)
 
-            #get number of elements with each A.M.
-            lens = [ len(c_l[k]) for k in range(lmax-lmin+1) ]
+            # get number of elements with each A.M.
+            lens = [len(c_l[k]) for k in range(lmax-lmin+1)]
 
-            #keep track of the order the orbitals are printed
-            elem.tags_permutations += [ x.tag for x in aos_set ]
+            # keep track of the order the orbitals are printed
+            elem.tags_permutations += [x.tag for x in aos_set]
 
-            #print n lmin lmax nexp nshell(lmin) nshell(lmin+1) ... nshell(lmax)
-            f.write('%i %i %i %i '\
-                    %(1, aos_set[0].l, aos_set[-1].l, len(exp_unique[i])))
-            for ln in lens: 
-                f.write('%i '%ln)
+            # print n lmin lmax nexp nshell(lmin) nshell(lmin+1) .. nshell(lmax)
+            f.write('%i %i %i %i '
+                    % (1, aos_set[0].l, aos_set[-1].l, len(exp_unique[i])))
+            for ln in lens:
+                f.write('%i ' % ln)
             f.write('\n')
-            #print exponent coeffs(lmin), ..., coeff(lmax)
+            # print exponent coeffs(lmin), ..., coeff(lmax)
             for j in range(len(exp_unique[i])):
-                f.write('%f '%(exp_unique[i][j]))
+                f.write('%f ' % (exp_unique[i][j]))
                 for l in range(lmax-lmin+1):
                     for k in range(lens[l]):
-                        f.write('%f '%c_l[l][k][j])
+                        f.write('%f ' % c_l[l][k][j])
                 f.write('\n')
-    
-    def __get_index_of_elem(self, atomic_nr):
+
+    def get_index_of_elem(self, atomic_nr):
         '''
         @brief return the index (in elemset) of the basis for atomic_nr.
         @param atomic_nr Atomic number.
@@ -326,12 +335,12 @@ class Basis():
         return self.chem_elems_indx.get(atomic_nr)
 
 
-#basis = Basis('STO-6G', 'STO-6G')
-#basis.__read_dalton_basisfile()
-#basis.__print_cp2k_basisfile('cp2k_STO-6G')
+# basis = Basis('STO-6G', 'STO-6G')
+# basis.__read_dalton_basisfile()
+# basis.__print_cp2k_basisfile('cp2k_STO-6G')
 
-#print(len(basis.chem_elems))
-#for e in basis.chem_elems[int(sys.argv[1])].ao:
+# print(len(basis.chem_elems))
+# for e in basis.chem_elems[int(sys.argv[1])].ao:
 #    print(e.l)
 #    print(e.c)
 #    print(e.e)
--- ReadMOsFromFile.py
+++ ReadMOsFromFile.py
@@ -2,6 +2,7 @@
 import sys
 import basis
 import IO
+from SimulationInfo import System
 
 try:
     import numpy as np
@@ -56,10 +57,9 @@ class MOinfo():
         self.filename = filename
         self.int_type = int_type
         self.double_type = double_type
+        # Read restart file
 
-        self.read_restart_file_cp2k_format()
-
-    def read_restart_file_cp2k_format(self, ):
+    def read_restart_file_cp2k_format(self):
         '''
         @brief Load CP2K input file.
         @date 2014
@@ -74,7 +74,7 @@ class MOinfo():
         # Read general info.
         [i_natom, i_nspin, i_nao, i_nsetmax, i_nshellmax] = read_record('INT', 5)
         print([i_natom, i_nspin, i_nao, i_nsetmax, i_nshellmax])
-        nsetinfo = read_record('INT', i_natom)
+        # nsetinfo = read_record('INT', i_natom)
         nshell_info = read_record('INT', i_nsetmax*i_natom)
         nshell_info.reshape(i_nsetmax, i_natom)
         nso_info = read_record('INT', i_nshellmax*i_nsetmax*i_natom)
@@ -85,18 +85,18 @@ class MOinfo():
         for ispin in range(1, i_nspin+1):
             #IF (para_env%ionode.AND.(nmo > 0)) THEN
             [i_nmo_read, i_homo, i_lfomo, i_nelectron] =\
-                    read_record('INT', 4)
+                read_record('INT', 4)
 
-            #TODO assert (nmo_read >= nmo)
-            i_nmo = min(i_nmo, i_nmo_read) #??
+            # TODO assert (nmo_read >= nmo)
+            i_nmo = min(i_nmo, i_nmo_read)  # ??
 
-            tmp =  read_record('DOUBLE', i_nmo_read*2)
+            tmp = read_record('DOUBLE', i_nmo_read*2)
             dd_eig = tmp[0:i_nmo]
             dd_occ = tmp[i_nmo_read:i_nmo_read+i_nmo]
 
-            #open filestream f
+            # open filestream f
 
-            if self.rt_mos: #unres or res??
+            if self.rt_mos:  # unres or res??
                 for imat in range(2*ispin-1, 2*ispin+1):
                     for i in range(1, i_nmo+1):
 
@@ -304,6 +304,318 @@ The struct looks like this
 '''
 
 #  TYPE cp_fm_type
+#see CP2K cp_fm_types.F
+import sys
+import basis
+import IO
+
+try:
+    import numpy as np
+except:
+    print('\nError loading nympy library in python.')
+    print('Make sure that the library is available.\n')
+    raise
+
+'''
+@brief Read restart file in CP2K format and convert to LSDALTON format.
+@param
+@date 2014
+@author Karl R. Leikanger.
+'''
+
+class MOinfo():
+    '''
+    @brief
+    @param
+    @date 2014
+    @author Karl R. Leikanger.
+    '''
+
+    filename = ''
+    int_type = ''
+    double_type = ''
+    rt_mos = ''        #TODO should be input
+    i_nmo = 9999999999 #TODO should be input ?
+
+    '''
+    basis_name = ''
+    nspin = 0
+    nshell = []
+    l = []
+    nshell_info = []
+    nso_info = []
+
+    ...
+    '''
+
+    def __init__(self, filename, rt_mos = False, \
+            int_type = np.int32, \
+            double_type = np.float64):
+        '''
+        @brief The constructor also reads the CP2K datafile (filename)
+        @brief and loads the data from the CP2K simulation.
+        @param
+        @date 2014
+        @author Karl R. Leikanger.
+        '''
+        self.rt_mos = rt_mos
+        self.filename = filename
+        self.int_type = int_type
+        self.double_type = double_type
+        # Read restart file
+        # self.read_restart_file_cp2k_format()
+
+    def read_restart_file_cp2k_format(self, system, basis):
+        '''
+        @brief Load CP2K input file.
+        @date 2014
+        @author Karl R. Leikanger.
+
+        See CP2K:qs_mo_io:read_mos_restart_low
+        '''
+        read_record = IO.ReadFortranBinaryFile(\
+                self.int_type, self.double_type, self.filename\
+                ).read_record
+
+        # Read general info.
+        [i_natom, i_nspin, i_nao, i_nsetmax, i_nshellmax] = read_record('INT', 5)
+        print([i_natom, i_nspin, i_nao, i_nsetmax, i_nshellmax])
+        nsetinfo = read_record('INT', i_natom)
+        nshell_info = read_record('INT', i_nsetmax*i_natom)
+        nshell_info.reshape(i_nsetmax, i_natom)
+        nso_info = read_record('INT', i_nshellmax*i_nsetmax*i_natom)
+        nso_info.reshape(i_nshellmax, i_nsetmax, i_natom)
+
+        #read MO's
+        i_nmo = self.i_nmo
+        for ispin in range(1, i_nspin+1):
+            #IF (para_env%ionode.AND.(nmo > 0)) THEN
+            [i_nmo_read, i_homo, i_lfomo, i_nelectron] =\
+                read_record('INT', 4)
+
+            # TODO assert (nmo_read >= nmo)
+            i_nmo = min(i_nmo, i_nmo_read)  # ??
+
+            tmp = read_record('DOUBLE', i_nmo_read*2)
+            dd_eig = tmp[0:i_nmo]
+            dd_occ = tmp[i_nmo_read:i_nmo_read+i_nmo]
+
+            # open filestream f
+
+            if self.rt_mos:  # unres or res??
+                for imat in range(2*ispin-1, 2*ispin+1):
+                    for i in range(1, i_nmo+1):
+
+                        dd_vecbuff = read_record('DOUBLE', i_nao)
+                        '''
+                        see in qs_mo_io.F what to do with the data
+                        '''
+            else:
+                for i in range(0, i_nmo):
+
+                    dd_vecbuff = read_record('DOUBLE', i_nao)
+                    print(dd_vecbuff)
+
+                    atom_nr = 0  # XXX n elec(atom_nr) per atom
+                    a = system.atoms[atom_nr].a
+                    basis_indx = basis.get_index_of_elem(a)
+                    print(a)
+                    print(basis_indx)
+                    permutations = basis.elements[i].tags_permutations
+                    dd_vecbuff = [ dd_vecbuff[i] for i in permutations ]
+
+                    print(permutations)
+                    print(dd_vecbuff)
+
+                    #further
+
+                    #
+                    # Input info
+                    #
+                    '''
+                    for iatom in range(1, natom+1):
+                        get_atomic_kind from SimulationInfo
+                        get_qs_kind (??)
+                        if associated(orb_basis_set) then
+                            get_gto_basis from SimulationInfo.basis
+                            minbas=false
+                        elseif associated(dftb_parameter) then ?
+                            get_dftb_atom_param
+                            minbas=false
+                        else
+                            unknown basis set?
+
+                         IF (para_env%ionode) THEN
+
+                    '''
+                    #
+                    # Reorganize vecbuf
+                    #
+                    '''
+
+                  use_this = .TRUE.
+                  iset_read = 1
+                  DO iset=1,nset
+                   '''
+                  # Input from basis file.
+                  # the number of sets in basis, each contains
+                  # nexp * (lshell(lmin) + ... + nshell(lmax)) ao's
+                    '''
+                     ishell_read = 1
+                     IF(minbas) THEN
+                        nnshell = lmax+1
+                     ELSE
+                        nnshell = nshell(iset)
+                    '''
+                        # Input from basis file.
+                        # nshell input
+                    '''
+                     END IF
+                     DO ishell=1,nnshell
+                        IF(minbas) THEN
+                           lshell = ishell-1
+                        ELSE
+                           lshell = l(ishell,iset)
+                        END IF
+                        IF (iset_read > nset_info(iatom)) use_this = .FALSE.
+                        IF (use_this) THEN ! avoids out of bound access of the lower line if false
+                           IF (nso(lshell) == nso_info(ishell_read,iset_read,iatom)) THEN
+                              offset_read=offset_info(ishell_read,iset_read,iatom)
+                              ishell_read=ishell_read+1
+                              IF (ishell_read > nshell_info(iset,iatom)) THEN
+                                 ishell_read = 1
+                                 iset_read = iset_read+1
+                              END IF
+                           ELSE
+                              use_this = .FALSE.
+                           END IF
+                        END IF
+                        DO iso=1,nso(lshell)
+                           IF (use_this) THEN
+                              IF (offset_read-1+iso.LT.1 .OR. offset_read-1+iso.GT.nao_read) THEN
+                                 vecbuffer(1,irow)=0.0_dp
+                              ELSE
+                                 vecbuffer(1,irow)=vecbuffer_read(1,offset_read-1+iso)
+                              END IF
+                           ELSE
+                              vecbuffer(1,irow) = 0.0_dp
+                           END IF
+                           irow = irow + 1
+                        END DO
+                        use_this = .TRUE.
+                    '''
+                        #then set MO's
+                    '''
+                CALL mp_bcast(vecbuffer,source,group)
+                CALL cp_fm_set_submatrix(mos(ispin)%mo_set%mo_coeff,&
+                 vecbuffer,1,i,nao,1,transpose=.TRUE.,error=error)
+
+                    '''
+            #Read the rest of the nmo's if there are any...
+            '''
+            IF(nmo>0) THEN
+               DO i=nmo+1,nmo_read
+                    READ (rst_unit) vecbuffer_read
+
+             '''
+
+'''
+
+         #      IF (para_env%ionode) THEN
+         #         READ (rst_unit) vecbuffer
+         #      ELSE
+         #         vecbuffer(1,:) = 0.0_dp
+         #      END IF
+         #      CALL mp_bcast(vecbuffer,source,group)
+         #      CALL cp_fm_set_submatrix(rt_mos(imat)%matrix,&
+         #           vecbuffer,1,i,nao,1,transpose=.TRUE.,error=error)
+         #   END DO
+         #END DO
+         #ELSE
+			#  DO i=1,nmo
+         #   IF (para_env%ionode) THEN
+         #      READ (rst_unit) vecbuffer_read
+         #      ! now, try to assign the read to the real vector
+         #      ! in case the basis set changed this involves some guessing
+         #      irow=1
+         #      DO iatom=1,natom
+			#
+			#
+			#! Skip extra MOs if there any
+                    '''
+
+
+#	def print_restart_file_dalton_format(self):
+
+#		! *****************************************************************************
+
+
+
+
+#__debug__ set to?
+#assert a<b, 'errorstring'
+#moinfo = MOinfo('slettmeg.wfn')
+
+
+
+
+'''
+
+Below a function to read mo coeff from unit ires
+
+'''
+
+#!> \brief ...
+#!> \param mo_set ...
+#!> \param ires ...
+#!> \param error ...
+#! *****************************************************************************
+#  SUBROUTINE read_mo_set_basic (mo_set, ires, error)
+#
+#    TYPE(mo_set_type), POINTER               :: mo_set
+#    INTEGER, INTENT(in)                      :: ires
+#    TYPE(cp_error_type), INTENT(inout)       :: error
+#
+#    CHARACTER(LEN=*), PARAMETER :: routineN = 'read_mo_set_basic', &
+#      routineP = moduleN//':'//routineN
+#
+#    INTEGER                                  :: mao, mmo, nao, nmo
+#    LOGICAL                                  :: failure
+#
+#    mao = mo_set%nao
+#    mmo = mo_set%nmo
+#
+#    IF ((ires>0).AND.(nmo > 0)) THEN
+#       READ (ires) nao,nmo,mo_set%homo,mo_set%lfomo,mo_set%nelectron
+#       CALL cp_assert(mao==nao,cp_fatal_level,cp_assertion_failed,routineP,&
+#                         "Different size of basis set",error,failure)
+#       CALL cp_assert(mmo==nmo,cp_fatal_level,cp_assertion_failed,routineP,&
+#                         "Different number of MOs",error,failure)
+#       READ (ires) mo_set%eigenvalues(1:nmo),mo_set%occupation_numbers(1:nmo)
+#    END IF
+#    CALL cp_fm_read_unformatted(mo_set%mo_coeff,ires,error)
+#
+#  END SUBROUTINE read_mo_set_basic
+
+'''
+Read unformatted is
+'''
+
+#    CALL cp_fm_read_unformatted(mo_set%mo_coeff,ires,error)
+#to
+#SUBROUTINE cp_fm_read_unformatted(fm,unit,error)
+#    TYPE(cp_fm_type), POINTER                :: fm
+#...
+#...
+#
+#   DO j=1,ncol_global
+#      READ (unit) fm%local_data(:,j)
+#   END DO
+'''
+The struct looks like this
+'''
+
+#  TYPE cp_fm_type
 #!    PRIVATE
 #     CHARACTER(LEN=60) :: name
 #     INTEGER :: id_nr, ref_count, print_count
@@ -314,6 +626,28 @@ The struct looks like this
 #  END TYPE cp_fm_type
 
 
-#import sys
-#o = MOinfo(sys.argv[1])
+import sys
+o = MOinfo('Si_bulk8-RESTART.wfn')
+
+#!    PRIVATE
+#     CHARACTER(LEN=60) :: name
+#     INTEGER :: id_nr, ref_count, print_count
+#     LOGICAL :: use_sp
+#     TYPE(cp_fm_struct_type), POINTER :: matrix_struct
+#     REAL(KIND = dp), DIMENSION(:,:), POINTER :: local_data
+#     REAL(KIND = sp), DIMENSION(:,:), POINTER :: local_data_sp
+#  END TYPE cp_fm_type
+
+from basis import Basis
+basis = Basis('STO-3G') #inp: basisname
+basis.read_basisfile('STO-3G', 'LSDALTON') #inp: inputfile, inputformat
+basis.print_basisfile('cp2k_STO-3G', 'CP2K') #inp: output filename, outputformat
+
+from SimulationInfo import System
+system = System()
+system.read_system_info('H2.mol', 'LSDALTON')
+
+o = MOinfo('He_bulk2-RESTART.wfn')
+#convert_restart_file ,,
+o.read_restart_file_cp2k_format(system, basis)
 
